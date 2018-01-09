98c4620fa6e5232169fb1c65e40e63687325d13e
Ankush Deora <ankushdeora11@gmail.com>
2018-01-10 00:41:16 +0530

    Update File_Manager.php

--- File_Manager.php
+++ File_Manager.php
@@ -1,13 +1,12 @@
 <?php
 class FileManager
 {
-	
-Public $result;
+/*Public $result1;
 public function __construct()
 {
-	$this -> result = null;
+	$this -> result1 = null;
 }
-/*
+
 public function set_result($result)
 {
  $this ->result = $result;
@@ -16,18 +15,17 @@ public function get_result()
 {
 	return $this ->result;
 }*/
-function calling()
+function calling($opt_data)
 {
 
  $myfile = fopen("newfile.txt","w") or die("Unable to open file!");
  
- fwrite($myfile, $result);
+ fwrite($myfile, print_r($opt_data,true));
 fclose($myfile);
- 
+ //print_r ($opt_data);
 }
 }
 
 
 
 ?> 
-	
